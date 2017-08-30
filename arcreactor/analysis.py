faf37f4fd1c6c030d5c0cc77c04a566abb144d89
hgandhi2411 <hgandhi@ur.rochester.edu>
2017-08-30 15:20:04 -0400

    Analysis updated to plot data from Simulation

--- arcreactor/analysis.py
+++ arcreactor/analysis.py
@@ -3,7 +3,6 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
-import time
 
 plt.ion()
 
@@ -18,6 +17,8 @@ class Analyzer:
             return self.plot_reactors(simulation_state)
 
     def plot_reactors(self,simulation_state):
+        fig, axes = plt.subplots(3, sharex = True, sharey = True)
+        fig.axis([0,3600, 0, 2])
         x = self.start_time - simulation_state.time          #time
         #y = simulation_state.kinetics[0].mole_fraction
         labels = ['C2H5COOCH3', 'H2O', 'CH3COOH', 'C2H5OH']
@@ -25,12 +26,11 @@ class Analyzer:
         for i in range(3):
             y = simulation_state.kinetics[i].mole_fraction
             axes[i].set_title('Reactor {}'.format(i+1))
-            for j in range(3):
-                axes[i].plot(x,y[j], color = colors[i], label = labels[j])
+            for j in range(4):
+                axes[i].plot(x,y[j], color = colors[j], label = labels[j])
         axes[2].set_xlabel('Time (seconds)')
         axes[1].set_ylabel('Concentration (moles/dm3)')
         fig.legend()
         output = io.BytesIO()
         fig.savefig(output, format='jpg')
         return output.getvalue()
-
