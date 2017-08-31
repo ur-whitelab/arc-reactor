fb732f32cc2ff9fb91a3c0db72f617e1a72b6d2b
hgandhi2411 <hgandhi@ur.rochester.edu>
2017-08-31 06:57:22 -0400

    Updated plots

--- arcreactor/analysis.py
+++ arcreactor/analysis.py
@@ -3,14 +3,18 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
-plt.ion()
 
 class Analyzer:
 
     def __init__(self, start_time):
         self.plot_number = 1
         self.start_time = start_time
+        self.xdata = []
+        self.r1 = []
+        self.r2 = []
+        self.r3 = []
 
     def get_plot(self, i, simulation_state):
         if i == 0:
@@ -18,16 +22,27 @@ class Analyzer:
 
     def plot_reactors(self,simulation_state):
         fig, axes = plt.subplots(3, sharex = True, sharey = True)
-        fig.axis([0,3600, 0, 2])
-        x = self.start_time - simulation_state.time          #time
-        #y = simulation_state.kinetics[0].mole_fraction
+        fig.axis([0,600, 0, 2])
         labels = ['C2H5COOCH3', 'H2O', 'CH3COOH', 'C2H5OH']
         colors = ['b', 'g', 'r', 'y']
+        
+        x = self.start_time - simulation_state.time          #time
+        self.xdata.append(x)
         for i in range(3):
             y = simulation_state.kinetics[i].mole_fraction
-            axes[i].set_title('Reactor {}'.format(i+1))
+            if i == 0:
+                self.r1.append(y)
+                ydata = np.asarray(self.r1)
+            if i ==1:
+                self.r2.append(y)
+                ydata = np.asarray(self.r2)
+            if i == 2:
+                self.r3.append(y)
+                ydata = np.asarray(self.r3)
+            
             for j in range(4):
-                axes[i].plot(x,y[j], color = colors[j], label = labels[j])
+                axes[i].plot(xdata,ydata[:,j], color = colors[j], label = labels[j])
+
         axes[2].set_xlabel('Time (seconds)')
         axes[1].set_ylabel('Concentration (moles/dm3)')
         fig.legend()
--- arcreactor/simulation.py
+++ arcreactor/simulation.py
@@ -3,7 +3,7 @@ import asyncio
 import datetime as dt
 import time
 from scipy.integrate import odeint
-from .protobufs.kinetics_pb2 import SystemKinetics
+from .protobufs.kinetics_pb2 import *
 
 # Hydrolysis of ester - A psuedo first order chemical reaction
 # All reactors are equally sized and operate at the same temperature
@@ -20,10 +20,11 @@ class Simulation:
         self.start_time = start_time
 
     def calculate(self, simulation_state):
-        c = np.empty((self.reactor_number, 4))
+        c = [0,0,0]
         '''Calculates all the kinetics of the reactors'''
         c0 = self.molar_feed_rate / self.volumetric_feed_rates  # mol/dm3
         tau = self.reactor_volumes[0] / self.volumetric_feed_rates[0]
+        k = 0.01
         t_int = np.linspace(0, 3600, 3600000)  # simulate for one hour
 
         def rxn_d(c, t, k=0.01):
@@ -31,8 +32,7 @@ class Simulation:
 
         ca1 = odeint(rxn_d, c0[0], t_int)
         x1 = 1 - ca1 / c0[0]    #Conversion
-        x = int(time.time() * 1000 - self.start_time * 1000)
-
+        x = int(simulation_state.time * 1000 - self.start_time * 1000)
         # The following steps for calculating reactor concentrations can be looped later
         c[0] = np.asarray([ca1[x], ca1[x], c0[0] - ca1[x], c0[0] - ca1[x]])
         mf1 = c[0] / np.sum(c[0])
@@ -42,12 +42,16 @@ class Simulation:
         ca3 = ca2 / (1 + tau * k)
         c[2] = np.asarray([ca3[x], ca3[x], c0[0] - ca3[x], c0[0] - ca3[x]])
         mf3 = c[2] / np.sum(c[2])
-        temp = 'room temperature'
-        pressure =  '1atm'                      #Specific to this reaction
+        temp = 25
+        pressure =  1                      #Specific to this reaction
 
-        simulation_state.time = time.time()
+        simulation_state.time = int(time.time())
         for i in range(3):
-            simulation_state.kinetics.append([temp, pressure, c[i]]) 
+            rxr = ReactionKinetics()
+            rxr.temperature = temp
+            rxr.pressure = pressure
+            rxr.mole_fraction = c[i]
+            simulation_state.kinetics.add(rxr) 
 
         return simulation_state
 
