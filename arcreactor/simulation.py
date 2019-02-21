a05d918fed39d752dea5a899e790e5190c6699ac
Andrew White <white.d.andrew@gmail.com>
2019-02-20 20:31:43 -0500

    Added capability to alter volume, based on implemented volume dial.

--- arcreactor/simulation.py
+++ arcreactor/simulation.py
@@ -25,8 +25,7 @@ class Simulation:
         #For demo purposes, the values are fixed
         self.chemical_species = ['Benzene', 'EtBr', 'TEB', 'HBr']
         self.reactor_number = 0
-        self.reactor_volume = 200            # m3
-        self.volumetric_feed_rates = np.array([10, 10])     # m3/s
+        self.volumetric_feed_rates = np.array([10, 10])     # dm3/s
         self.molar_feed_rate = np.array([1, 1])           # mol/s
         self.start_time = start_time
         self.graph_time = 0
@@ -133,24 +132,29 @@ class Simulation:
                     simulation_state.kinetics[-1].id = node.id
                     self.ready_flags[node.id] = False #start not ready
                     if (len(node.weight) > 0):
+                        #print('from add_delete_protobuf_objects, node {} has temperature of {} and pressure of {}'.format(node.id, node.weight[0], node.weight[1]))#this prints 1.0 for pressure
                         simulation_state.kinetics[-1].temperature = node.weight[0]#T is always the first in this repeat field
+                        simulation_state.kinetics[-1].pressure = node.weight[1] #actually is volume TODO: change protobuf.
                     else:
                         simulation_state.kinetics[-1].temperature = 400  #default
+                        simulation_state.kinetics[-1].pressure = 200 #default; actually is volume. TODO: change protobuf.
         return simulation_state
 
 
-    def calc_conc(self, initial_conc, reactor_type, k_eq, k, id):
+    def calc_conc(self, initial_conc, V, reactor_type, k_eq, k, id):
         conc0 = self.molar_feed_rate / self.volumetric_feed_rates #molar_feed_rate is a list
         if(reactor_type == 'cstr'):
-            conc_limiting, ready = self.cstr(initial_conc, t=self.time, k_eq = k_eq, k = k)
+            conc_limiting, ready = self.cstr(initial_conc, V = V, k_eq = k_eq, k = k)
         elif(reactor_type == 'pfr'):
-            conc_limiting, ready = self.pfr(initial_conc, t=self.time, k_eq = k_eq, k = k, done_time = self.done_times[id])
+            conc_limiting, ready = self.pfr(initial_conc, V = V,  k_eq = k_eq, k = k, done_time = self.done_times[id])
+        elif(reactor_type == 'pbr'):
+            conc_limiting, ready = self.pbr(initial_conc, V = V,  k_eq = k_eq, k = k)
         else:
             conc_limiting = conc0[0]
         #print(conc_limiting)
         return (conc_limiting, ready)
 
-    def calc_outputs(self, id, simulation_state, R, P):
+    def calc_outputs(self, id, simulation_state, R):
         '''RECURSIVELY calculate output concentrations for each node in the graph. ALWAYS call with id 0 first!'''
         found = False #assume it's not there
         for kinetics in simulation_state.kinetics:#look for node with incoming connection from this ID
@@ -162,6 +166,7 @@ class Simulation:
                     e_act = 47000  #kJ/kmol
                     k_eq = 100000 * math.e ** (-33.78*(T-298)/T)    #equilibrium constant
                     k = 5*10**6 * math.exp(-e_act / (R * T))      # Rate constant, time dependence needs to be added
+                    V = kinetics.pressure #this is actually volume. TODO: Change protobuf.
                     #find the limiting concentration for the ith reactor
                     #conc_limiting = self.calc_conc(sum([conc_out[idx] for idx in self.edge_list_in[i]]), kinetics.label, kinetics.id, k_eq, k)
                     conc_out_sum = 0.0
@@ -183,15 +188,17 @@ class Simulation:
                             max_done_time_in = max(max_done_time_in, self.done_times[idx])
                         conc_out_sum /= vol_out_sum# (C1V1 + C2V2)/(V1+V2) = C_final
                         conc_product /= vol_out_sum
-                        if(kinetics.label == 'cstr'):
+                        if(kinetics.label == 'cstr' or kinetics.label == 'pbr'):
                             self.done_times[kinetics.id] = max_done_time_in + 0.0
                         elif(kinetics.label == 'pfr'):
                             if self.vol_in_rates[kinetics.id] > 0:
-                                self.done_times[kinetics.id] = max_done_time_in + self.reactor_volume/self.vol_in_rates[kinetics.id]
+                                #self.done_times[kinetics.id] = max_done_time_in + self.reactor_volume/self.vol_in_rates[kinetics.id]
+                                self.done_times[kinetics.id] = max_done_time_in + V/self.vol_in_rates[kinetics.id]
                             else:
-                                self.done_times[kinetics.id] = max_done_time_in + self.reactor_volume/self.volumetric_feed_rates[1]
+                                #self.done_times[kinetics.id] = max_done_time_in + self.reactor_volume/self.volumetric_feed_rates[1]
+                                self.done_times[kinetics.id] = max_done_time_in + V/self.volumetric_feed_rates[1]
                         #print('conc_out_sum is {} and conc_product is {} for id {}'.format(conc_out_sum, conc_product, kinetics.id))
-                        conc_limiting, self.ready_flags[kinetics.id] = self.calc_conc(initial_conc = conc_out_sum, reactor_type=kinetics.label,  k_eq=k_eq, k=k, id=kinetics.id)
+                        conc_limiting, self.ready_flags[kinetics.id] = self.calc_conc(initial_conc = conc_out_sum, V=V, reactor_type=kinetics.label, k_eq=k_eq, k=k, id=kinetics.id)
                         #print('Conc limiting for reactor id {} is {}'.format(kinetics.id, conc_limiting))
 
                         #conc = [conc_limiting, conc_limiting, conc_product + (conc_out_sum - conc_limiting), conc_product + (conc_out_sum - conc_limiting)]
@@ -202,7 +209,7 @@ class Simulation:
                         self.conc_out_reactant[kinetics.id] = 0.0
                         self.conc_out_product[kinetics.id] = 0.0
                         self.ready_flags[kinetics.id] = False
-                    self.calc_outputs(kinetics.id, simulation_state, R, P)#now that this node has its outputs set, go on to the ones it outputs to
+                    self.calc_outputs(kinetics.id, simulation_state, R)#now that this node has its outputs set, go on to the ones it outputs to
 
         if(not found):
             return
@@ -225,7 +232,7 @@ class Simulation:
         #it may be necessary to add an "else" part of this if statement. This is because the simulation_state.chemical_species
         #will not be None, but it will potentially not be the correct chemical species either.
 
-        if(not self.connected_to_source ):
+        if(not self.connected_to_source):
             self.start_plotting = False
             if( self.graph_time % 20 == 0):
                 print('NOT CONNECTED TO SOURCE')
@@ -251,7 +258,6 @@ class Simulation:
             #print('reactors = {}'.format(self.reactor_number))
 
         R = 8.314      # Universal gas constant (kJ/kmol K)
-        P = 1          # Pressure (atm)
 
 
         self.time = self.graph_time - self.start_time
@@ -261,8 +267,7 @@ class Simulation:
         for kinetics in simulation_state.kinetics:
             self.conc_out_reactant[kinetics.id] = 0
             self.conc_out_product[kinetics.id] = 0
-            kinetics.pressure = P
-        self.calc_outputs(id = 0, simulation_state = simulation_state, R = R, P = P)
+        self.calc_outputs(id = 0, simulation_state = simulation_state, R = R)
         #start with ID zero to look for nodes connected to source
         #print('simulation_state.kinetics is {}'.format(simulation_state.kinetics))
         #print('the keys for conc_out are {}, and the keys for self.edge_list_in are {}'.format(conc_out.keys(), self.edge_list_in.keys()))
@@ -286,60 +291,52 @@ class Simulation:
         return simulation_state
 
 
-    def cstr(self, initial_conc, t, k_eq = 5, k = 0.1):
+    def cstr(self, initial_conc, V, k_eq = 5, k = 0.1): #TODO: Need to create bool (or similar) to prevent CSTR from working when a gaseous reaction.
         '''Calculates concentrations for a first order, reversible reaction in a CSTR.
-
         Parameters
         ----------
         initial_conc : float
-                    Concentration of limiting reactant entering the reactor
-        i : int
-            Reactor id
+            Concentration of limiting reactant entering the reactor
+        V : float
+            Denotes the total volume of the reactor
         k_eq : float
             Denotes equilibrium concentration of the reaction
         k : float
             Denotes the reaction constant for the forward reaction
-
         Returns
         -------
         float
             Final concentration of the limiting reactor when it leaves the reactor
         '''
-        #def rate(conv, t):
-        #   return -k* initial_conc * (1 - (1 + self.c / self.a /k_eq)*conv)
-        rv = self.reactor_volume #m3
+
         fa0 = self.molar_feed_rate[0] #mol/s
-        conversion = min(rv * k * initial_conc/(fa0 + k* rv * initial_conc + (self.c / self.a * k * rv * initial_conc)/k_eq), 1.0)
+        conversion = min(V * k * initial_conc/(fa0 + k* V * initial_conc + (self.c / self.a * k * V * initial_conc)/k_eq), 1.0)
         #print('Conversion from cstr is {} at {}'.format(conversion, self.time))
         out_conc_lr = initial_conc*(1.0 - conversion)
         ready = True #CSTR is instantaneous
         #print('A left in cstr is {}'.format(out_conc_lr))
         return (out_conc_lr, ready)
 
-    def pfr(self, initial_conc, t, k_eq = 5, k = 0.1, done_time = None):
+    def pfr(self, initial_conc, V, k_eq = 5, k = 0.1, done_time = None):
         '''Calculates concentrations for a first order, reversible reaction in a PFR.
-
         Parameters
         ----------
         initial_conc : float
-                    Concentration of limiting reactant entering the reactor
-        t: int
-            Time in simulation at which concentration needs to be calculated
+            Concentration of limiting reactant entering the reactor
+        V : float
+            Denotes the total volume of the reactor
         k_eq : float
             Denotes equilibrium concentration of the reaction
         k : float
             Denotes the reaction constant for the forward reaction
-
         Returns
         -------
         float
                 Final concentration of the limiting reactant when it leaves the reactor
         '''
+
         if(done_time is None):
-            done_time = self.reactor_volume/self.volumetric_feed_rates[1]
-        #def rate(conv, t):
-        #    return -k * initial_conc*(1 - (1 + self.c / self.a /k_eq) * conv)
-        #conversion = min(si.odeint(rate, 0.0001, np.arange(0, 3600, 3600*25)), 1.0)  # ~25fps
+            done_time = V/self.volumetric_feed_rates[1]
         time = min(done_time, self.time/3.25)
         ready = False
         if(self.time/3.25 >= done_time):
@@ -349,3 +346,29 @@ class Simulation:
         out_conc_lr = initial_conc*(1.0 - conversion)
         #print('A left in pfr is {}'.format(out_conc_lr))
         return (out_conc_lr, ready)
+
+    # def pbr(self, initial_conc, V, k_eq = 5, k = 0.1):
+    #     '''Calculates concentrations for a first order, reversible reaction in a PBR.
+    #     Parameters
+    #     ----------
+    #     initial_conc : float
+    #         Concentration of limiting reactant entering the reactor
+    #     V : float
+    #         Denotes the total volume of the reactor
+    #     t: int
+    #         Time in simulation at which concentration needs to be calculated
+    #     k_eq : float
+    #         Denotes equilibrium concentration of the reaction
+    #     k : float
+    #         Denotes the reaction constant for the forward reaction
+    #     Returns
+    #     -------
+    #     float
+    #             Final concentration of the limiting reactant when it leaves the reactor
+    #     '''
+
+    #     conversion = min((math.exp(V * k * (1 + self.c / self.a / k_eq)/self.vol_in_rates[kinetics.id]) + 1)/(1 + self.c / self.a / k_eq), 1)
+    #     #print('Conversion from pbr is {} at {}'.format(conversion, t))
+    #     out_conc_lr = initial_conc * (1.0 - conversion)
+    #     #print('A left in pbr is {}'.format(out_conc_lr))
+    #     return (out_conc_lr, ready)
